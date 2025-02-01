218727af5562faccf267f337ded67ca2bde1ff0a
br3cht <ant_175@hotmail.com>
2025-02-01 19:58:44 -0300

    refactor: created web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\SiteController;
+use App\Http\Controllers\SoccerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/competions', [SoccerController::class, 'getCompetions']);
+Route::get('/standing', [SiteController::class, 'matches']);
